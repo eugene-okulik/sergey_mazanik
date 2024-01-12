05e525bac54c1cc214c7faae874960bc555c3505
Siarhei Mazanik <sergey.mazanik1@gmail.com>
2024-01-12 22:51:13 +0300

    homework_3 done

---
+++ homework/sergey_mazanik/homework_3/task_1.py
@@ -0,0 +1,8 @@
+# Даны 2 числа a и b. Получить их сумму, разность и произведение
+
+a = int(input())
+b = int(input())
+summa = a + b
+sub = a - b
+multi = a * b
+print(f'Сумма: {a} + {b} = {summa}\nРазность: {a} - {b} = {sub}\nУмножение: {a} * {b} = {multi}')
---
+++ homework/sergey_mazanik/homework_3/task_2.py
@@ -0,0 +1,5 @@
+# Даны числа x и y. Получить x − y / 1 + xy
+
+x = int(input())
+y = int(input())
+print(x - y / 1 + x * y)
